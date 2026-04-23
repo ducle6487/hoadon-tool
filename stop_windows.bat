496187d85847e6b14e3929f7eb06eb8f7c0d50d2
ducle <ducle6487@gmail.com>
2026-04-23 10:33:51 +0700

    Add stop_windows.bat to easily terminate instances on Windows

---
+++ stop_windows.bat
@@ -0,0 +1,18 @@
+@echo off
+CHCP 65001 >NUL
+Title Tat Auto Hoa Don Tool
+
+echo ==============================================================
+echo        DANG DONG CA HA HE THONG...
+echo ==============================================================
+echo.
+
+echo Dang tat server uvicorn...
+taskkill /F /IM python.exe /T >nul 2>&1
+taskkill /F /IM uvicorn.exe /T >nul 2>&1
+
+echo.
+echo ==============================================================
+echo  [HOAN TAT] Da huy toan bo cac server dang chay ngam!
+echo ==============================================================
+pause
